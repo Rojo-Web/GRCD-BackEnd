ad6caaf7cc95a027f5a1d3259ad6a43c90718616
Rojo-Web <santifa2004@gmail.com>
2024-05-19 01:34:16 -0500

    controlador api de clases

--- app/Http/Controllers/api/clasesController.php
+++ app/Http/Controllers/api/clasesController.php
@@ -5,6 +5,10 @@ namespace App\Http\Controllers\api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use App\Models\clase;
+use \Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\DB;
+
 class clasesController extends Controller
 {
     /**
@@ -12,7 +16,12 @@ class clasesController extends Controller
      */
     public function index()
     {
-        //
+        $clases = DB::table('clases')
+            ->join('instalaciones', 'clases.instalacion_id', '=', 'instalaciones.id')
+            ->join('entrenadores', 'clases.entrenador_id', '=', 'entrenadores.id')
+            ->select('clases.*', 'entrenadores.nombre as entrenadore_nombre', 'instalaciones.nombre as instalaciones_nombre', 'entrenadores.apellido as entrenadore_apellido')
+            ->get();
+            return json_encode(['clases' => $clases]);
     }
 
     /**
@@ -20,7 +29,31 @@ class clasesController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validate = Validator::make($request->all(),[
+            'nombre'=> ['required'],
+            'descripcion'=> ['required'],
+            'instalacion_id'=> ['required','number'],
+            'entrenador_id'=> ['required','number'],
+            'hora_inicio'=> ['required'],
+            'duracion'=> ['required','number'],
+        ]);
+
+        if($validate->fails()){
+            return response()->json([
+                'msg'=> 'Se produjo un error en la validacion de la informacion ',
+                'statusCode'=> 400
+            ]);
+        }
+        $clase = new clase();
+        $clase->instalacion_id = $request->instalacion_id;
+        $clase->entrenador_id = $request->entrenador_id;
+        $clase->hora_inicio = $request->hora_inicio;
+        $clase->duracion = $request->duracion;
+        $clase->nombre = $request->nombre;
+        $clase->descripcion = $request->descripcion;
+        $clase->id = $request->id;
+        $clase->save();
+        return json_encode(['clase' => $clase,'success'=>true]);
     }
 
     /**
@@ -28,7 +61,18 @@ class clasesController extends Controller
      */
     public function show(string $id)
     {
-        //
+        $clase = clase::find($id);
+        if (is_null($clase)){
+            return abort(404);
+        }
+
+        $instalaciones = DB::table('instalaciones')
+            ->orderBy('nombre')
+            ->get();
+        $entrenadores = DB::table('entrenadores')
+            ->orderBy('nombre')
+            ->get();
+        return json_encode(['clase' => $clase,"instalaciones" => $instalaciones,"entrenadores" => $entrenadores]);
     }
 
     /**
@@ -36,7 +80,18 @@ class clasesController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $clase = clase::find($id);
+        if (is_null($clase)){
+            return abort(404);
+        }
+        $clase->instalacion_id = $request->instalacion_id;
+        $clase->entrenador_id = $request->entrenador_id;
+        $clase->hora_inicio = $request->hora_inicio;
+        $clase->duracion = $request->duracion;
+        $clase->nombre = $request->nombre;
+        $clase->descripcion = $request->descripcion;
+        $clase->save();
+        return json_encode(['clase' => $clase,'success'=>true]); //
     }
 
     /**
@@ -44,6 +99,11 @@ class clasesController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $clase = clase::find($id);
+        if (is_null($clase)){
+            return abort(404);
+        }
+        $clase->delete();
+        return json_encode(['clase' => $clase,'success'=>true]);
     }
 }
