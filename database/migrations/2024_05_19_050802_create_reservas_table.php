de488ef301b187d2138d9db8959b01e0c9b70cf8
Rojo-Web <santifa2004@gmail.com>
2024-05-19 00:28:19 -0500

    migraciones

---
+++ database/migrations/2024_05_19_050802_create_reservas_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reservas', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('instalacion_id')->nullable()->references('id')->on('instalaciones');
+            $table->foreignId('cliente_id')->nullable()->references('id')->on('clientes');
+            $table->timestamps('fecha_hora_inicio');
+            $table->timestamps('fecha_hora_final');
+            $table->string('estado');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reservas');
+    }
+};
---
+++ database/migrations/2024_05_19_050850_create_clases_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('clases', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->text('descripcion');
+            $table->foreignId('instalacion_id')->nullable()->references('id')->on('instalaciones');
+            $table->foreignId('entrenador_id')->nullable()->references('id')->on('entrenadores');
+            $table->timestamps('hora_inicio');
+            $table->integer('duracion');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('clases');
+    }
+};
