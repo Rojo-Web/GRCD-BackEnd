4a9320262520a671a772dfaf34a0ef19bc49a85a
Rojo-Web <santifa2004@gmail.com>
2024-05-19 01:00:46 -0500

    controlador api instalaciones

--- app/Http/Controllers/api/instalacionesController.php
+++ app/Http/Controllers/api/instalacionesController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-use App\Models\categorie;
+use App\Models\instalacion;
 use \Illuminate\Support\Facades\Validator;
 
 class instalacionesController extends Controller
@@ -15,8 +15,8 @@ class instalacionesController extends Controller
      */
     public function index()
     {
-        // $categories = categorie::all();
-        // return json_encode(['categories' => $categories]);
+        $instalaciones = instalacion::all();
+        return json_encode(['instalaciones' => $instalaciones]);
     }
 
     /**
@@ -24,7 +24,27 @@ class instalacionesController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validate = Validator::make($request->all(),[
+            'nombre'=> ['required'],
+            'tipo'=> ['required'],
+            'capacidad'=> ['required','min:10'],
+            'disponibilidad'=> ['required'],
+        ]);
+
+        if($validate->fails()){
+            return response()->json([
+                'msg'=> 'Se produjo un error en la validacion de la informacion ',
+                'statusCode'=> 400
+            ]);
+        }
+        $instalacion = new instalacion();
+        $instalacion->nombre = $request->nombre;
+        $instalacion->tipo = $request->tipo;
+        $instalacion->capacidad = intval($request->capacidad);
+        $instalacion->disponibilidad = $request->disponibilidad;
+        $instalacion->id = $request->id;
+        $instalacion->save();
+        return json_encode(['instalacion' => $instalacion,'success'=>true]);
     }
 
     /**
@@ -32,7 +52,11 @@ class instalacionesController extends Controller
      */
     public function show(string $id)
     {
-        //
+        $instalacion = instalacion::find($id);
+        if (is_null($instalacion)){
+            return abort(404);
+        }
+        return json_encode(['instalacion' => $instalacion]);
     }
 
     /**
@@ -40,7 +64,16 @@ class instalacionesController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $instalacion = instalacion::find($id);
+        if (is_null($instalacion)){
+            return abort(404);
+        }
+        $instalacion->nombre = $request->nombre;
+        $instalacion->tipo = $request->tipo;
+        $instalacion->capacidad = intval($request->capacidad);
+        $instalacion->disponibilidad = $request->disponibilidad;
+        $instalacion->save();
+        return json_encode(['instalacion' => $instalacion,'success'=>true]);
     }
 
     /**
@@ -48,6 +81,12 @@ class instalacionesController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $instalacion = instalacion::find($id);
+        if (is_null($instalacion)){
+            return abort(404);
+        }
+        $instalacion = instalacion::find($id);
+        $instalacion->delete();
+        return json_encode(['instalacion' => $instalacion,'success'=>true]);
     }
 }
